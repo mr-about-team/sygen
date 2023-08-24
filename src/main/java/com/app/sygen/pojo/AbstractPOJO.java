ba6f4079087f6a3ad1574ada9c1ade12c53115b3
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-08-24 16:48:56 +0200

    patch: ajout de la class AbstractPOJO
    les pojo permette d'avoir les modeles non persistés pour facilité la manipulation

---
+++ src/main/java/com/app/sygen/pojo/AbstractPOJO.java
@@ -0,0 +1,8 @@
+package com.app.sygen.pojo;
+
+import java.io.Serializable;
+
+public abstract class AbstractPOJO implements Serializable 
+{
+    protected static final Long serialVersionUID = 1L;
+}
