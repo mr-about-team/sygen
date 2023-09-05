f731529cdd91e8003ca9c764dc27a846db794cdc
Jerry <jerryjephte@gmail.com>
2023-09-05 09:04:51 +0100

    {chore}service deliberation

---
+++ src/main/java/com/app/sygen/services/serviceDeliberation/Datacontrolleur.java
@@ -0,0 +1,16 @@
+package com.app.sygen.services.serviceDeliberation;
+
+import java.util.List;
+
+public class Datacontrolleur{
+    private String filiere;
+    private List<CritereDeliberation> tableData;
+
+    public String getFiliere() {
+        return filiere;
+    }
+    public List<CritereDeliberation> getTableData() {
+        return tableData;
+    }
+
+}
